a928f9db21e2a3dc6077b4cf9c1aa2af750b218b
sbengo <serigna@gmail.com>
2016-11-08 17:06:25 +0100

    fix issue 30

--- public/common/validation.service.ts
+++ public/common/validation.service.ts
@@ -42,10 +42,12 @@ export class ValidationService {
     static OIDValidator(control) {
         // {6,100}           - Assert password is between 6 and 100 characters
         // (?=.*[0-9])       - Assert a string has at least one number
-        if (control.value.match(/^\./) || control.value == "") {
-            return null;
-        } else {
-            return { 'invalidOID': true };
+        if (control.value){
+            if (control.value.match(/^\./) || control.value == "") {
+                return null;
+            } else {
+                return { 'invalidOID': true };
+            }
         }
     }
 }
