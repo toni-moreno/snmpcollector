06b91eefdc46b8c06dc8bf203eeb0695b83d8fed
toni-moreno <toni.moreno@gmail.com>
2016-11-12 01:55:31 +0100

    Go virtual machine self monitor metrics now in one measurement, metric names are closer now to the go api runtime names

--- pkg/selfmon.go
+++ pkg/selfmon.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"github.com/influxdata/influxdb/client/v2"
-	"os"
 	"runtime"
 	"strings"
 	"sync"
@@ -19,6 +18,7 @@ type SelfMonConfig struct {
 	Influx              *InfluxDB
 	runtimeStatsRunning bool
 	TagMap              map[string]string
+	Fields              map[string]interface{}
 	bps                 *client.BatchPoints
 }
 
@@ -28,29 +28,33 @@ func (sm *SelfMonConfig) Init() {
 		sm.TagMap = make(map[string]string)
 		for _, tag := range sm.ExtraTags {
 			s := strings.Split(tag, "=")
-			key, value := s[0], s[1]
-			sm.TagMap[key] = value
+			if len(s) == 2 {
+				key, value := s[0], s[1]
+				sm.TagMap[key] = value
+			} else {
+				log.Errorf("Error on tag  definition TAG=VALUE [ %s ] for SelfMon", tag)
+			}
 		}
 	}
-}
-
-func (sm *SelfMonConfig) Report(metric string, value float64, timestamp time.Time) error {
-	pt, _ := client.NewPoint(
-		metric,
-		sm.TagMap,
-		map[string]interface{}{
-			"value": value,
-		},
-		timestamp,
-	)
-
-	(*sm.bps).AddPoint(pt)
-	return nil
+	//Init Measurment Fields.
+	sm.Fields = map[string]interface{}{
+		"runtime_goroutines":    0.0,
+		"mem.alloc":             0.0,
+		"mem.mallocs":           0.0,
+		"mem.frees":             0.0,
+		"mem.heapAlloc":         0.0,
+		"mem.stackInuse":        0.0,
+		"gc.total_pause_ns":     0.0,
+		"gc.pause_per_second":   0.0,
+		"gc.pause_per_interval": 0.0,
+		"gc.gc_per_second":      0.0,
+		"gc.gc_per_interval":    0.0,
+	}
 }
 
 func (sm *SelfMonConfig) ReportStats(wg *sync.WaitGroup) {
 	if sm.runtimeStatsRunning {
-		fmt.Fprintf(os.Stderr, "Runtime stats is already running\n")
+		log.Error("Runtime stats is already running")
 		return
 	}
 
@@ -78,18 +82,18 @@ func (sm *SelfMonConfig) reportRuntimeStats(sleep time.Duration, wg *sync.WaitGr
 
 		now := time.Now()
 
-		sm.Report(fmt.Sprintf("%s.goroutines", prefix), float64(runtime.NumGoroutine()), now)
-		sm.Report(fmt.Sprintf("%s.memory.allocated", prefix), float64(memStats.Alloc), now)
-		sm.Report(fmt.Sprintf("%s.memory.mallocs", prefix), float64(memStats.Mallocs), now)
-		sm.Report(fmt.Sprintf("%s.memory.frees", prefix), float64(memStats.Frees), now)
-		sm.Report(fmt.Sprintf("%s.memory.gc.total_pause", prefix), float64(memStats.PauseTotalNs)/nsInMs, now)
-		sm.Report(fmt.Sprintf("%s.memory.heap", prefix), float64(memStats.HeapAlloc), now)
-		sm.Report(fmt.Sprintf("%s.memory.stack", prefix), float64(memStats.StackInuse), now)
+		sm.Fields["runtime_goroutines"] = float64(runtime.NumGoroutine())
+		sm.Fields["mem.alloc"] = float64(memStats.Alloc)
+		sm.Fields["mem.mallocs"] = float64(memStats.Mallocs)
+		sm.Fields["mem.frees"] = float64(memStats.Frees)
+		sm.Fields["gc.total_pause_ns"] = float64(memStats.PauseTotalNs) / nsInMs
+		sm.Fields["mem.heapAlloc"] = float64(memStats.HeapAlloc)
+		sm.Fields["mem.stackInuse"] = float64(memStats.StackInuse)
 
 		if lastPauseNs > 0 {
 			pauseSinceLastSample := memStats.PauseTotalNs - lastPauseNs
-			sm.Report(fmt.Sprintf("%s.memory.gc.pause_per_second", prefix), float64(pauseSinceLastSample)/nsInMs/sleep.Seconds(), now)
-			sm.Report(fmt.Sprintf("%s.memory.gc.pause_per_interval", prefix), float64(pauseSinceLastSample)/nsInMs, now)
+			sm.Fields["gc.pause_per_second"] = float64(pauseSinceLastSample) / nsInMs / sleep.Seconds()
+			sm.Fields["gc.pause_per_interval"] = float64(pauseSinceLastSample) / nsInMs
 		}
 		lastPauseNs = memStats.PauseTotalNs
 
@@ -97,14 +101,13 @@ func (sm *SelfMonConfig) reportRuntimeStats(sleep time.Duration, wg *sync.WaitGr
 		if lastNumGc > 0 {
 			diff := float64(countGc)
 			diffTime := now.Sub(lastSampleTime).Seconds()
-			sm.Report(fmt.Sprintf("%s.memory.gc.gc_per_second", prefix), diff/diffTime, now)
-			sm.Report(fmt.Sprintf("%s.memory.gc.gc_per_interval", prefix), diff, now)
-
+			sm.Fields["gc.gc_per_second"] = diff / diffTime
+			sm.Fields["gc.gc_per_interval"] = diff
 		}
 
 		if countGc > 0 {
 			if countGc > 256 {
-				fmt.Fprintf(os.Stderr, "We're missing some gc pause times")
+				log.Warn("We're missing some gc pause times")
 				countGc = 256
 			}
 			var totalPause float64 = 0
@@ -114,12 +117,23 @@ func (sm *SelfMonConfig) reportRuntimeStats(sleep time.Duration, wg *sync.WaitGr
 				totalPause += pause
 				//	sm.Report(fmt.Sprintf("%s.memory.gc.pause", prefix), pause/nsInMs, now)
 			}
-			sm.Report(fmt.Sprintf("%s.memory.gc.pause_per_interval", prefix), totalPause/nsInMs, now)
+			//sm.Report(fmt.Sprintf("%s.memory.gc.pause_per_interval", prefix), totalPause/nsInMs, now)
+			sm.Fields["gc.pause_per_interval"] = totalPause / nsInMs
 		}
 
 		lastNumGc = memStats.NumGC
 		lastSampleTime = now
-
+		metricname := "selmon_gvm"
+		if len(prefix) > 0 {
+			metricname = fmt.Sprintf("%sselfmon_gvm", prefix)
+		}
+		pt, _ := client.NewPoint(
+			metricname,
+			sm.TagMap,
+			sm.Fields,
+			now,
+		)
+		(*sm.bps).AddPoint(pt)
 		//BatchPoint Send
 		sm.Influx.Send(sm.bps)
 
