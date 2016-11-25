6dd6e353c6d19809ff2b9c945db7cdd03c805faa
sbengo <serigna@gmail.com>
2016-11-25 19:06:15 +0100

    enhanced custom_modal component, removed counterfield input

--- public/common/generic-modal.ts
+++ public/common/generic-modal.ts
@@ -7,28 +7,40 @@ import { ModalDirective } from 'ng2-bootstrap/components/modal/modal.component';
     selector: 'test-modal',
     template: `
       <div bsModal #childModal="bs-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-lg">
+          <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" (click)="childModal.hide()" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
                 <h4 class="modal-title" *ngIf="myObject != null">{{titleName}} <b>{{ myObject.ID }}</b></h4>
-                <h5 class="modal-title" *ngIf="myObject != null" >
-                <span *ngFor="let field of countFields">
-                  <span>{{field}}:{{myObject[field] != null ? myObject[field].length : '0' }}
-                  </span>
-                   </span>
-                </h5>
               </div>
               <div class="modal-body">
-                <div *ngFor="let entry of myObject | objectParser">
-                  <dl class="dl-horizontal" *ngIf="entry.value !='' && entry.value != null ">
-                    <dt>{{ entry.key }}</dt>
-                    <dd>{{ entry.value }}</dd>
+                <div *ngIf = "myObject">
+                  <div [ngClass]="empty ? customMessageClass[0] : (customMessageClass[1] || customMessageClass[0])" role="alert" *ngIf="customMessage">
+                    <span class="glyphicon glyphicon-exclamation-sign"></span>
+                    {{empty ? customMessage[0] : (customMessage[1] || customMessage[0])}}
+                  </div>
+                </div>
+                <div  *ngFor="let entry of myObject | objectParser">
+                  <dl class="dl-horizontal" *ngIf="entry.value !='' && entry.value != null">
+                    <dt>{{entry.key}} <label *ngIf="isArray(entry.value)" class="label label-primary" style="display: inline-table; margin:0px">{{ entry.value.length}}</label></dt>
+                    <dd *ngIf="!isArray(entry.value)">
+                      {{entry.value}}
+                    </dd>
+                    <div *ngIf="isArray(entry.value)" style="margin-bottom:10px">
+                      <dd *ngFor="let val of entry.value">{{val}}</dd>
+                    </div>
+                    <dt *ngIf = "entry.value.Description">Description</dt>
+                    <dd *ngIf = "entry.value.Description">{{entry.value.Description}}</dd>
+                    <hr>
                   </dl>
                 </div>
               </div>
+              <div class="modal-footer" *ngIf="showValidation === true">
+               <button type="button" class="btn btn-default" (click)="childModal.hide()">Close</button>
+               <button type="button" class="btn btn-primary" (click)="validationClick(myObject.ID)">{{textValidation ? textValidation : Save}}</button>
+             </div>
             </div>
           </div>
         </div>`
@@ -36,14 +48,35 @@ import { ModalDirective } from 'ng2-bootstrap/components/modal/modal.component';
 
 export class GenericModal {
   @ViewChild('childModal') public childModal: ModalDirective;
-  @Input() countFields : any;
   @Input() titleName : any;
+  @Input() customMessage: string;
+  @Input() showValidation: boolean;
+  @Input() textValidation: string;
+  @Input() customMessageClass: string;
+  @Input() controlSize: boolean;
 
+  @Output() public validationClicked:EventEmitter<any> = new EventEmitter();
+
+  public validationClick(myId: string):void {
+    this.validationClicked.emit(myId);
+  }
 
   constructor() { }
   myObject: any = null;
+  empty : any = false;
+
   parseObject( myObject : any){
     this.myObject = myObject;
+    this.empty = (Object.keys(myObject).length > 1);
     this.childModal.show();
   }
+
+  isArray (myObject) {
+    return myObject instanceof Array;
+  }
+
+  hide() {
+    this.childModal.hide();
+  }
+
 }
