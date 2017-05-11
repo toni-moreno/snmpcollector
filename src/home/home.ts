dbdcbc2f2136ab101d0695a7ab4781c8149bb70f
toni-moreno <toni.moreno@gmail.com>
2017-05-11 23:08:48 +0200

    fix reload conf reload crash ouside runtime view

--- src/home/home.ts
+++ src/home/home.ts
@@ -91,7 +91,7 @@ export class Home {
 
   reloadConfig() {
     this._blocker.start(this.container, "Reloading Conf. Please wait...");
-    this.rt.updateRuntimeInfo(null,null,false)
+    if (this.rt) this.rt.updateRuntimeInfo(null,null,false);
     this.homeService.reloadConfig()
     .subscribe(
     response => {
