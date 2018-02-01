5a777b91e00d1178cf9c4d513ed5b26770d513ea
sbengo <serigna@gmail.com>
2018-02-01 17:26:56 +0100

    UI exported MeasFilters table functions on table-list component

--- src/measfilter/measfiltercfg.component.ts
+++ src/measfilter/measfiltercfg.component.ts
@@ -17,6 +17,9 @@ import { ItemsPerPageOptions } from '../common/global-constants';
 import { TableActions } from '../common/table-actions';
 import { AvailableTableActions } from '../common/table-available-actions';
 
+import { TableListComponent } from '../common/table-list.component';
+import { MeasFilterCfgComponentConfig, TableRole, OverrideRoleActions } from './measfiltercfg.data';
+
 declare var _:any;
 
 @Component({
@@ -31,7 +34,6 @@ export class MeasFilterCfgComponent {
   @ViewChild('viewModalDelete') public viewModalDelete: GenericModal;
   @ViewChild('exportFileModal') public exportFileModal : ExportFileModal;
 
-  editEnabled : boolean = false;
   selectedArray : any = [];
   public isRequesting : boolean;
   public counterItems : number = null;
@@ -47,7 +49,9 @@ export class MeasFilterCfgComponent {
   influxmeas: Array<any>;
   selectmeas: IMultiSelectOption[] = [];
   selectCustomFilters:  IMultiSelectOption[] = [];
-
+  public defaultConfig : any = MeasFilterCfgComponentConfig;
+  public tableRole : any = TableRole;
+  public overrideRoleActions: any = OverrideRoleActions;
   oidconditions: Array<any>;
   selectoidcond: IMultiSelectOption[] = [];
 
@@ -60,13 +64,6 @@ export class MeasFilterCfgComponent {
   //Initialization data, rows, colunms for Table
   private data: Array<any> = [];
   public rows: Array<any> = [];
-  public columns: Array<any> = [
-    { title: 'ID', name: 'ID' },
-    { title: 'Measurement ID', name: 'IDMeasurementCfg' },
-    { title: 'Filter Type', name: 'FType' },
-    { title: 'Filter Name', name: 'FilterName' },
-    { title: 'EnableAlias', name: 'EnableAlias' }
-  ];
 
   public page: number = 1;
   public itemsPerPage: number = 20;
@@ -78,7 +75,7 @@ export class MeasFilterCfgComponent {
   //Set config
   public config: any = {
     paging: true,
-    sorting: { columns: this.columns },
+    sorting: { columns: this.defaultConfig['table-columns'] },
     filtering: { filterString: '' },
     className: ['table-striped', 'table-bordered']
   };
@@ -87,13 +84,6 @@ export class MeasFilterCfgComponent {
     this.editmode = 'list';
     this.reloadData();
     this.builder = builder;
-  }
-
-  enableEdit() {
-    this.editEnabled = !this.editEnabled;
-    console.log(this.editEnabled);
-    let obsArray = [];
-    this.tableAvailableActions = new AvailableTableActions('measfilter').availableOptions;
   }
 
   createStaticForm() {
@@ -148,14 +138,8 @@ export class MeasFilterCfgComponent {
     this.createDynamicForm(controlArray);
   }
 
-  onResetFilter() : void {
-    this.page = 1;
-    this.myFilterValue = "";
-    this.config.filtering = {filtering: { filterString: '' }};
-    this.onChangeTable(this.config);
-  }
-
-  applyAction(test : any) : void {
+  applyAction(test : any, data? : Array<any>) : void {
+    this.selectedArray = data || [];
     switch(test.action) {
        case "RemoveAllSelected": {
           this.removeAllSelectedItems(this.selectedArray);
@@ -167,105 +151,26 @@ export class MeasFilterCfgComponent {
     }
   }
 
-  public changePage(page: any, data: Array<any> = this.data): Array<any> {
-    //Check if we have to change the actual page
-    let maxPage =  Math.ceil(data.length/this.itemsPerPage);
-    if (page.page > maxPage && page.page != 1) this.page = page.page = maxPage;
-
-    let start = (page.page - 1) * page.itemsPerPage;
-    let end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : data.length;
-    return data.slice(start, end);
-  }
-
-  public changeSort(data: any, config: any): any {
-    if (!config.sorting) {
-      return data;
+  customActions(action : any) {
+    switch (action.option) {
+      case 'export' : 
+        this.exportItem(action.event);
+      break;
+      case 'new' :
+        this.newMeasFilter()
+      case 'view':
+        this.viewItem(action.event);
+      break;
+      case 'edit':
+        this.editMeasFilter(action.event);
+      break;
+      case 'remove':
+        this.removeItem(action.event);
+      break;
+      case 'tableaction':
+        this.applyAction(action.event, action.data);
+      break;
     }
-
-    let columns = this.config.sorting.columns || [];
-    let columnName: string = void 0;
-    let sort: string = void 0;
-
-    for (let i = 0; i < columns.length; i++) {
-      if (columns[i].sort !== '' && columns[i].sort !== false) {
-        columnName = columns[i].name;
-        sort = columns[i].sort;
-      }
-    }
-
-    if (!columnName) {
-      return data;
-    }
-
-    // simple sorting
-    return data.sort((previous: any, current: any) => {
-      if (previous[columnName] > current[columnName]) {
-        return sort === 'desc' ? -1 : 1;
-      } else if (previous[columnName] < current[columnName]) {
-        return sort === 'asc' ? -1 : 1;
-      }
-      return 0;
-    });
-  }
-
-  public changeFilter(data: any, config: any): any {
-    let filteredData: Array<any> = data;
-    this.columns.forEach((column: any) => {
-      if (column.filtering) {
-        filteredData = filteredData.filter((item: any) => {
-          return item[column.name].match(column.filtering.filterString);
-        });
-      }
-    });
-
-    if (!config.filtering) {
-      return filteredData;
-    }
-
-    if (config.filtering.columnName) {
-      return filteredData.filter((item: any) =>
-        item[config.filtering.columnName].match(this.config.filtering.filterString));
-    }
-
-    let tempArray: Array<any> = [];
-    filteredData.forEach((item: any) => {
-      let flag = false;
-      this.columns.forEach((column: any) => {
-        if (item[column.name] === null) {
-          item[column.name] = '--'
-        }
-        if (item[column.name].toString().match(this.config.filtering.filterString)) {
-          flag = true;
-        }
-
-      });
-      if (flag) {
-        tempArray.push(item);
-      }
-    });
-    filteredData = tempArray;
-
-    return filteredData;
-  }
-
-  changeItemsPerPage (items) {
-    this.itemsPerPage = parseInt(items);
-    let maxPage =  Math.ceil(this.length/this.itemsPerPage);
-    if (this.page > maxPage) this.page = maxPage;
-    this.onChangeTable(this.config);
-  }
-
-  public onChangeTable(config: any, page: any = { page: this.page, itemsPerPage: this.itemsPerPage }): any {
-    if (config.filtering) {
-      Object.assign(this.config.filtering, config.filtering);
-    }
-    if (config.sorting) {
-      Object.assign(this.config.sorting, config.sorting);
-    }
-    let filteredData = this.changeFilter(this.data, this.config);
-    let sortedData = this.changeSort(filteredData, this.config);
-    this.rows = page && config.paging ? this.changePage(page, sortedData) : sortedData;
-    this.length = sortedData.length;
   }
 
   reloadData() {
@@ -278,7 +183,6 @@ export class MeasFilterCfgComponent {
         this.isRequesting = false;
         this.measfilters = data;
         this.data = data;
-        this.onChangeTable(this.config);
       },
       err => console.error(err),
       () => console.log('DONE')
@@ -289,7 +193,7 @@ export class MeasFilterCfgComponent {
     this.reloadData();
   }
 
-  viewItem(id, event) {
+  viewItem(id) {
     console.log('view', id);
     this.viewModal.parseObject(id);
   }
